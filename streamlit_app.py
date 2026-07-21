38101093f3212ec482471f282d1c6000c5374621
Vatsal0025 <vatsalpatel0025@gmail.com>
2026-07-21 23:59:38 +0530

    artifacts added

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 import pandas as pd
 
-API_URL = "http://127.0.0.1:8000/predict"
+API_URL = "http://3.93.60.206/predict"
 
 st.title(
     "Drug-Induced Gene Expression Prediction"
